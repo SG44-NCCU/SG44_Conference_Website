a54914ab05e5cd2ff69bd756d716db182c9a6da9
CJuuuuuuu <110207404@g.nccu.edu.tw>
2026-01-28 23:10:10 +0800

    fix: 首頁News更新問題

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,4 +1,5 @@
 // 移除 Navbar 和 Footer 的 import
+export const dynamic = 'force-dynamic'
 import Hero from '@/components/sections/home/Hero'
 import NewsSection from '@/components/sections/home/NewsSection'
 import TimelineSection from '@/components/sections/home/TimelineSection'
@@ -6,15 +7,15 @@ import TopicGrid from '@/components/sections/home/TopicGrid'
 
 export default function PublicPage() {
   return (
-    <div className="bg-white"> 
+    <div className="bg-white">
       {/* 這裡不用再寫 Navbar 了，因為 layout 已經有了 */}
       {/* 這裡原本的 <main> 也可以拿掉，因為 layout 已經包了一層 main */}
-      
+
       <Hero />
       <TimelineSection />
       <NewsSection />
       <TopicGrid />
-      
+
       {/* Footer 也不用了 */}
     </div>
   )
